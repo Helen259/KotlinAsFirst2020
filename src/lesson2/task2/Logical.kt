56f1c79f845ea9e896691519a6ecf7a4655b3c22
Елена <fedorovalena2002@yandex.ru>
2020-09-23 10:57:31 +0300

    :)

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -44,7 +44,7 @@ fun daysInMonth(month: Int, year: Int): Int = when (month) {
     1, 3, 5, 7, 8, 10, 12 -> 31
     4, 6, 9, 11 -> 30
     else -> {
-        if ((month == 2) && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) 29
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 29
         else 28
     }
 }
