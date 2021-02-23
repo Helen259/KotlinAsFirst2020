12b124915653f28820b90ca52c4941d73287d481
Елена <fedorovalena2002@yandex.ru>
2021-02-23 07:01:56 +0300

    :)

--- src/lesson11/task1/Polynom.kt
+++ src/lesson11/task1/Polynom.kt
@@ -2,6 +2,9 @@
 
 package lesson11.task1
 
+import kotlin.math.max
+import kotlin.math.pow
+
 /**
  * Класс "полином с вещественными коэффициентами".
  *
@@ -20,16 +23,26 @@ package lesson11.task1
  * Старшие коэффициенты, равные нулю, игнорировать, например Polynom(0.0, 0.0, 5.0, 3.0) соответствует 5x+3
  */
 class Polynom(vararg coeffs: Double) {
+    private val list = if (coeffs.all { it == 0.0 }) listOf(0.0) else coeffs.toMutableList().dropWhile { it == 0.0 }
+
+    constructor(list: List<Double>) : this(*list.toDoubleArray())
 
     /**
      * Геттер: вернуть значение коэффициента при x^i
      */
-    fun coeff(i: Int): Double = TODO()
+
+    fun coeff(i: Int): Double = list[i]
 
     /**
      * Расчёт значения при заданном x
      */
-    fun getValue(x: Double): Double = TODO()
+    fun getValue(x: Double): Double {
+        var value = 0.0
+        for (i in list.indices) {
+            value += list[i] * x.pow(list.size - i - 1)
+        }
+        return value
+    }
 
     /**
      * Степень (максимальная степень x при ненулевом слагаемом, например 2 для x^2+x+1).
@@ -38,27 +51,55 @@ class Polynom(vararg coeffs: Double) {
      * Слагаемые с нулевыми коэффициентами игнорировать, т.е.
      * степень 0x^2+0x+2 также равна 0.
      */
-    fun degree(): Int = TODO()
+    fun degree(): Int {
+        for (i in list.indices) {
+            return list.size - i - 1
+        }
+        return 0
+    }
 
     /**
      * Сложение
      */
-    operator fun plus(other: Polynom): Polynom = TODO()
+    operator fun plus(other: Polynom): Polynom {
+        val list1 = this.list.reversed()
+        val list2 = other.list.reversed()
+        val res = MutableList(max(list.size, other.list.size)) { 0.0 }
+        for (i in res.indices) {
+            if (i in list2.indices)
+                res[i] += list2[i]
+            if (i in list1.indices)
+                res[i] += list1[i]
+        }
+        val res2 = res.reversed()
+        return Polynom(res2)
+    }
+
 
     /**
      * Смена знака (при всех слагаемых)
      */
-    operator fun unaryMinus(): Polynom = TODO()
+    operator fun unaryMinus(): Polynom = Polynom(list.map { -it })
+
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Polynom): Polynom = TODO()
+    operator fun minus(other: Polynom): Polynom = this.plus(other.unaryMinus())
+
 
     /**
      * Умножение
      */
-    operator fun times(other: Polynom): Polynom = TODO()
+    operator fun times(other: Polynom): Polynom {
+        val res = MutableList(this.degree() + other.degree() + 1) { 0.0 }
+        this.list.forEachIndexed { ithis, value1 ->
+            other.list.forEachIndexed { iother, value2 ->
+                res[ithis + iother] += value1 * value2
+            }
+        }
+        return Polynom(res)
+    }
 
     /**
      * Деление
@@ -68,20 +109,65 @@ class Polynom(vararg coeffs: Double) {
      *
      * Если A / B = C и A % B = D, то A = B * C + D и степень D меньше степени B
      */
-    operator fun div(other: Polynom): Polynom = TODO()
+    operator fun div(other: Polynom): Polynom {
+        val res = mutableListOf<Double>()
+        var divisible = this.list
+        when {
+            this.degree() < other.degree() -> throw IllegalArgumentException()
+            other == Polynom(0.0) -> throw IllegalArgumentException()
+        }
+        while (divisible.size >= other.list.size) {
+            val c = divisible.first() / other.list.first()
+            res.add(c)
+            val m = other.list.map { it * -c }
+            val list = List(divisible.size - other.list.size) { 0.0 }
+            val list2 = m + list
+            divisible = divisible.zip(list2).map { it.first + it.second }.toMutableList()
+            divisible.removeAt(0)
+        }
+        return Polynom(res)
+    }
+
 
     /**
      * Взятие остатка
      */
-    operator fun rem(other: Polynom): Polynom = TODO()
+    operator fun rem(other: Polynom): Polynom {
+        when {
+            this.degree() < other.degree() -> throw IllegalArgumentException()
+            other == Polynom(0.0) -> throw IllegalArgumentException()
+        }
+        return this - (this / other) * other
+    }
+
+    /* {val res = mutableListOf<Double>()
+        var divisible = this.list
+        when {
+            this.degree() < other.degree() -> throw IllegalArgumentException()
+            other == Polynom(0.0) -> throw IllegalArgumentException()
+        }
+        while (divisible.size >= other.list.size) {
+            val c = divisible.first() / other.list.first()
+            res.add(c)
+            val m = other.list.map { it * -c }
+            val list = List(divisible.size - other.list.size) { 0.0 }
+            val list2 = m + list
+            divisible = divisible.zip(list2).map { it.first + it.second }.toMutableList()
+            divisible.removeAt(0)
+        }
+        return Polynom(divisible)
+    }*/
+
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Polynom && this.list == other.list
+
 
     /**
      * Получение хеш-кода
      */
-    override fun hashCode(): Int = TODO()
+    override fun hashCode(): Int = list.hashCode()
 }
+
--- test/lesson11/task1/PolynomTest.kt
+++ test/lesson11/task1/PolynomTest.kt
@@ -19,6 +19,10 @@ class PolynomTest {
     fun getValue() {
         val p = Polynom(1.0, 3.0, 2.0)
         assertEquals(42.0, p.getValue(5.0), 1e-10)
+        val l = Polynom(0.0, 3.0, 2.0)
+        assertEquals(17.0, l.getValue(5.0), 1e-10)
+        val lm = Polynom(0.0)
+        assertEquals(0.0, lm.getValue(5.0), 1e-10)
     }
 
     @Test
@@ -30,6 +34,12 @@ class PolynomTest {
         assertEquals(0, q.degree())
         val r = Polynom(0.0, 1.0, 2.0)
         assertEquals(1, r.degree())
+        val u = Polynom(0.0, 0.0, 2.0)
+        assertEquals(0, u.degree())
+        val w = Polynom(1.0, 0.0)
+        assertEquals(1, w.degree())
+        val j = Polynom(0.0, 3.0, 0.0, 2.0)
+        assertEquals(2, j.degree())
     }
 
     @Test
@@ -39,7 +49,18 @@ class PolynomTest {
         val p2 = Polynom(1.0, 3.0, 2.0)
         val r = Polynom(1.0, -1.0, 2.0, 6.0)
         assertApproxEquals(r, p1 + p2, 1e-10)
-        assertApproxEquals(r, p2 + p1, 1e-10)
+        val g1 = Polynom(0.0, 1.0, -1.0, 4.0)
+        val g2 = Polynom(0.0, 3.0, 2.0)
+        val l = Polynom(0.0, 1.0, 2.0, 6.0)
+        assertApproxEquals(l, g1 + g2, 1e-10)
+        val a1 = Polynom(4.0)
+        val a2 = Polynom(1.0, 3.0, 2.0)
+        val p = Polynom(1.0, 3.0, 6.0)
+        assertApproxEquals(p, a1 + a2, 1e-10)
+        val o1 = Polynom(0.0)
+        val o2 = Polynom(0.0)
+        val w = Polynom(0.0)
+        assertApproxEquals(w, o1 + o2, 1e-10)
     }
 
     @Test
@@ -68,6 +89,14 @@ class PolynomTest {
         val r = Polynom(1.0, 1.0, -5.0, -3.0, 10.0, 8.0)
         assertApproxEquals(r, p1 * p2, 1e-10)
         assertApproxEquals(r, p2 * p1, 1e-10)
+        val s1 = Polynom(0.0)
+        val s2 = Polynom(1.0, 3.0, 2.0)
+        val t = Polynom(0.0)
+        assertApproxEquals(t, s1 * s2, 1e-10)
+        val a1 = Polynom(6.0, 1.0)
+        val a2 = Polynom(2.0, 5.0, 3.0)
+        val e = Polynom(12.0, 32.0, 23.0, 3.0)
+        assertApproxEquals(e, a1 * a2, 1e-10)
     }
 
     @Test
@@ -77,7 +106,13 @@ class PolynomTest {
         val p2 = Polynom(1.0, 3.0, 2.0)
         val r = Polynom(1.0, -5.0)
         assertApproxEquals(r, p1 / p2, 1e-10)
+        assertApproxEquals(Polynom(0.0), Polynom(0.0) / Polynom(2.0), 1e-10)
         assertApproxEquals(Polynom(1.0, 2.0), Polynom(2.0, 4.0) / Polynom(0.0, 2.0), 1e-10)
+        assertApproxEquals(Polynom(2.0, 0.0), Polynom(2.0, 4.0, 2.0) / Polynom(0.0, 1.0, 2.0), 1e-10)
+        assertApproxEquals(Polynom(2.0), Polynom(2.0, 4.0, 2.0) / Polynom(1.0, 0.0, 2.0), 1e-10)
+        assertApproxEquals(Polynom(1.0, 1.0), Polynom(2.0, 2.0, 1.0) / Polynom(2.0, 0.0), 1e-10)
+        assertApproxEquals(Polynom(1.0, 1.0), Polynom(2.0, 2.0, 1.0) / Polynom(0.0, 2.0, 0.0), 1e-10)
+        assertApproxEquals(Polynom(1.0), Polynom(6.0, 0.0, 5.2) / Polynom(6.0, 0.0, 5.2), 1e-10)
     }
 
     @Test
@@ -89,6 +124,21 @@ class PolynomTest {
         val q = Polynom(12.0, 14.0)
         assertApproxEquals(q, p1 % p2, 1e-10)
         assertApproxEquals(p1, p2 * r + q, 1e-10)
+
+        val a1 = Polynom(2.0, 2.0, 1.0)
+        val a2 = Polynom(2.0, 0.0)
+        val e = Polynom(1.0, 1.0)
+        val w = Polynom(1.0)
+        assertApproxEquals(w, a1 % a2, 1e-10)
+        assertApproxEquals(a1, a2 * e + w, 1e-10)
+
+        val c1 = Polynom(0.0, 2.0, 1.0)
+        val c2 = Polynom(0.0, 2.0)
+        val u = Polynom(1.0, 0.5)
+        val j = Polynom(0.0)
+        assertApproxEquals(j, c1 % c2, 1e-10)
+        assertApproxEquals(c1, c2 * u + j, 1e-10)
+
     }
 
     @Test
